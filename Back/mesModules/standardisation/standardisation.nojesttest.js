748e7fa84f538d9a9f9d56e18709f86bb99c8f2b
Fatih Bozlak <bozlak.fatih@gmail.com>
2022-08-24 15:07:06 +0200

    correction des testes pour la nouvelle défintion de la fonction toStandardise

--- Back/mesModules/standardisation/standardisation.nojesttest.js
+++ Back/mesModules/standardisation/standardisation.nojesttest.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const sdd = require("../carnetsEntretiens/initCarnetsEntretiensSdd")();
 const toStandardiser = require("./standardisation");
 
 let aTester = null;
@@ -8,9 +9,8 @@ aTester = aTester.toString().split("\r\n");
 let nbATester = aTester.length;
 let nbStandardise = 0;
 
-
 aTester.forEach(tester => {
-  toStandardiser(tester) ? nbStandardise++ : null;
+  toStandardiser(sdd, tester) ? nbStandardise++ : null;
 })
 
 console.log(`Sur ${nbATester}, ${nbStandardise} ont été standardisé`);
