8272ce6c7a6096f87b6c3650de9c56a893594d23
Fatih Bozlak <bozlak.fatih@gmail.com>
2022-08-24 08:47:47 +0200

    Initialisation de la sdd au démarrage du serveur

--- Back/index.js
+++ Back/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = 5000;
+
+const initCarnetsEntretiensSdd = require(path.join(__dirname, 'mesModules/carnetsEntretiens/initCarnetsEntretiensSdd'));
+const carnetsEntretiensSdd = initCarnetsEntretiensSdd();
 
 app.get('/', (_, resp) => {
   resp.send("ok");
