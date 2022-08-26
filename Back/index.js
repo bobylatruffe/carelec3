1d713fb395be9d272f132a3286bdea0aebd71ea8
Fatih Bozlak <bozlak.fatih@gmail.com>
2022-08-26 08:02:41 +0200

    Initialisation du serveur NodeJs, création de la route pour définir les API agissant sur la sdd des carnets d'entretiens

--- Back/index.js
+++ Back/index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const router = express.Router();
 const PORT = 5000;
 
-const initCarnetsEntretiensSdd = require(path.join(__dirname, 'mesModules/carnetsEntretiens/initCarnetsEntretiensSdd'));
-const carnetsEntretiensSdd = initCarnetsEntretiensSdd();
+// initialiser notre structure de donnée pour les préconisations constructeurs
+const { initCarnetsEntretiensSdd } = require("./mesModules/carnetsEntretiens/initCarnetsEntretiensSdd");
+initCarnetsEntretiensSdd();
 
 app.get('/', (_, resp) => {
   resp.send("ok");
+})
+
+const carnetsEntretiensRoute = require("./routes/carnetsEntretiens/carnetsEntretiens");
+app.use("/api/carnetsEntretiens", carnetsEntretiensRoute);
+
+app.get("/*", (_, resp) => { 
+  return resp.status(404).send("404");
 })
 
 app.listen(PORT, () => {
