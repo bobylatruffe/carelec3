97832f210ddb2ab1f5c54827731fe5fb2d06896a
Fatih Bozlak <bozlak.fatih@gmail.com>
2022-08-27 20:13:03 +0200

    Fonctionnalité pour visualiser les defauts de la voiture pour l'user; la possibilité d'ajouter des défauts pour le garagiste 'l'admin'

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,11 +1,14 @@
 import { Fragment } from "react";
+
 import ResumerCurrentRevision from "./components/ResumerCurrentRevision/ResumerCurrentRevision";
+import EtatDesLieux from "./components/EtatDesLieux/EtatDesLieux";
 
 function App() {
   return (
     <Fragment>
       <h1>Carelec</h1>
       <ResumerCurrentRevision />
+      <EtatDesLieux type="pickUp" admin={true} />
     </Fragment>
   );
 }
--- Front/src/utilitaires/apiServeur.js
+++ Front/src/utilitaires/apiServeur.js
@@ -1,4 +1,44 @@
 /*
+  Mettre à jour l'état des lieux d'une révision courante.
+*/
+function setCurrentRevisionEdl(id, type, croixRouges) {
+  let toJsonSend = {
+    type,
+    newValue: {
+      realiserLe: new Date().toLocaleString("fr-FR").replace(/\/:/g, ''),
+      croixRouges: [croixRouges]
+    }
+  }
+
+  return fetch(`http://localhost:5000/api/admin/${id}/currentRevisionEdl`, {
+    method: "POST",
+    body: JSON.stringify(toJsonSend),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(resp => {
+      if (resp.status === 500) {
+        return Promise.reject("Erreur: setCurrentRevisionEdl()");
+      }
+
+      return resp.json();
+    })
+}
+
+/* 
+  Retourne simplement la ppt etatDesLieux  de la révision courante de l'user id.
+*/
+function getCurrentResumerEdl(id) {
+  return getCurrentResumer(id)
+    .then(currentRevision => {
+      currentRevision = currentRevision[Object.keys(currentRevision)[0]];
+      return currentRevision.etatDesLieux;
+    })
+    .catch(err => console.log("Erreur: getCurrentResumerEdl()"));
+}
+
+/*
   Retourne une version simplifier de la révision courante de l'user id.
 */
 function getCurrentResumerSimple(id) {
@@ -27,4 +67,4 @@ function getCurrentResumer(id) {
     })
 }
 
-export { getCurrentResumer, getCurrentResumerSimple };
+export { getCurrentResumer, getCurrentResumerSimple, getCurrentResumerEdl, setCurrentRevisionEdl };
